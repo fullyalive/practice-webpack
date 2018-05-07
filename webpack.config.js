0bdc7070a333987d8488c8e86c32388f10d4a792
fullyalive <wogud1202@gmail.com>
2018-05-07 17:45:56 +0900

    adding module to webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,17 @@ module.exports = {
     entry: "./index.js",
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
+        filename: "bundle.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: "babel-loader",
+                options: {
+                    presets: ['es2015']
+                }
+            }
+        ]
     }
 };
